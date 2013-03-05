aea6b6d5bf286b94a81d08c0da5a81560cde2a38
Eric Sauter <esauter5@gmail.com>
2013-03-05 14:52:26 -0500

    Refactored some code. Need to finish/fix game logic

--- RubyWebDev/blackjack.rb
+++ RubyWebDev/blackjack.rb
@@ -1,59 +1,127 @@
-ranks = %w{ 2 3 4 5 6 7 8 9 10 Jack Queen King Ace }
-suits = %w{ Diamonds Hearts Spades Clubs }
 
-deck = ranks.product(suits).map{|card| card.join(" of ")}.shuffle
+def initialize_values
+	cardValues = Hash.new(0)
+	(2..10).each {|i| cardValues[i.to_s.to_sym] = i }
+	cardValues[:J] = 10
+	cardValues[:Q] = 10
+	cardValues[:K] = 10
+	cardValues[:A] = 11
 
-#puts deck
+	cardValues
+end
 
-cardValues = Hash.new(0)
+def generate_deck
+	ranks = %w{ 2 3 4 5 6 7 8 9 10 Jack Queen King Ace }
+	suits = %w{ Diamonds Hearts Spades Clubs }
+	deck = ranks.product(suits).map{|card| card.join(" of ")}.shuffle
+end
 
-(2..10).each {|i| cardValues[i.to_s.to_sym] = i }
-cardValues[:J] = 10
-cardValues[:Q] = 10
-cardValues[:K] = 10
-cardValues[:A] = 11
+def deal_card(hand,deck)
+	hand << deck.pop
+end
 
-playerCards = []
-dealerCards = []
-playerCards << deck.pop
-dealerCards << deck.pop
-playerCards << deck.pop
-dealerCards << deck.pop
+def print_player_cards(playerCards)
+	playerCards.each_with_index do |v,i| 
+	puts "Player Card #{i+1}: #{v}" 
+	#playerScore += cardValues[v[0].to_sym]
+end
+end
+
+def print_dealer_cards(dealerCards, playerStay)
+	dealerCards.each_with_index do |v,i| 
+		if i == 0 && playerStay == false
+			puts "Dealer Card #{i+1}: Hidden" 
+		else
+			puts "Dealer Card #{i+1}: #{v}" 
+		end
+	end
+end
+
+def hand_value(hand, cardValues)
+	score = 0
+
+	hand.each_with_index do |v,i|
+		if score > 10 && v[0] == "A"
+			score += 1
+		else 
+			score += cardValues[v[0].to_sym]
+		end
+	end
+
+	score
+end
+
+def determine_winner(playerScore, dealerScore)
+	if playerScore > dealerScore
+		puts "You win!!!"
+	elsif dealerScore > playerScore
+		puts "Dealer wins!!!"
+	else
+		puts "Tie!!!"
+	end
+end
+
 playerScore = 0
 dealerScore = 0
 
 playerStay = false
+playerBust = false
 
-playerCards.each_with_index do |v,i| 
-	puts "Player Card #{i+1}: #{v}" 
-	playerScore += cardValues[v[0].to_sym]
-end
+cardValues = initialize_values
 
-dealerCards.each_with_index do |v,i| 
-	if i == 0
-		puts "Dealer Card #{i+1}: Hidden" 
-	else
-		puts "Dealer Card #{i+1}: #{v}" 
-	end
-end
+deck = generate_deck
+
+playerCards = []
+dealerCards = []
+
+2.times { deal_card(playerCards, deck) }
+2.times { deal_card(dealerCards, deck) }
+
+playerScore = hand_value(playerCards, cardValues)
+dealerScore = hand_value(dealerCards, cardValues)
 
 
 
-until playerStay == true
+print_player_cards(playerCards)
+print_dealer_cards(dealerCards,playerStay)
+
+puts "Player score is: #{playerScore}"
+
+
+until playerStay == true || playerBust == true
 	puts "Would you like to hit or stay?"
 
 	case gets.chomp
-		when 'hit'
-			card = deck.pop
-			puts card
-			playerScore += cardValues[card[0].to_sym]
-			playerCards << card
-			puts "Player Score: #{playerScore}"
-		when 'stay'
-			playerStay = true
+	when 'hit'
+		deal_card(playerCards,deck)
+		playerScore = hand_value(playerCards,cardValues)
+		print_player_cards(playerCards)
+		#playerScore += cardValues[card[0].to_sym]
+		#playerCards << card
+		puts "Player Score: #{playerScore}"
+
+		playerBust = true if playerScore > 21
+	when 'stay'
+		playerStay = true
 	end
 end
 
-#puts deck
+	if playerBust == true
+		puts "You BUSTED!!! Dealer WINS!!!"
+	elsif playerStay == true
+		until dealerScore >= 17
+			deal_card(dealerCards, deck)
+			dealerScore = hand_value(dealerCards, cardValues)
+		end
+
+		print_dealer_cards(dealerCards, playerStay)
+		puts "Dealer Score: #{dealerScore}"
+
+		if dealerScore > 21
+			puts "Dealer BUSTED!!! You win!!!"
+		else
+			determine_winner(playerScore, dealerScore)
+		end
+	end
 
 
