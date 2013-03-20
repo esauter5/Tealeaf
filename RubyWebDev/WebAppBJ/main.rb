d90749dd396b05128baa2c024ce9be94cad63d7c
Eric Sauter <esauter5@gmail.com>
2013-03-19 22:50:39 -0400

    Name form and correct routing.

--- RubyWebDev/WebAppBJ/main.rb
+++ RubyWebDev/WebAppBJ/main.rb
@@ -4,7 +4,24 @@ require 'sinatra'
 set :sessions, true
 
 get '/' do
-  erb :test
+  if  session[:name]
+    redirect '/game'
+  else
+    redirect '/new_player'
+  end
+end
+
+get '/game' do
+  erb :welcome
+end
+
+get '/new_player' do
+  erb :name_form
+end
+
+post '/new_player' do
+  session[:name] = params[:name]
+  redirect '/'
 end
 
 
