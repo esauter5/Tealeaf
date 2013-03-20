b2aa7e1efd8948bee799d7d2df19a0217a46d162
Eric Sauter <esauter5@gmail.com>
2013-03-20 17:16:16 -0400

    Began building game functionality. Hit and stay works with images

--- RubyWebDev/WebAppBJ/main.rb
+++ RubyWebDev/WebAppBJ/main.rb
@@ -12,7 +12,19 @@ get '/' do
 end
 
 get '/game' do
-  erb :welcome
+  ranks = %w{ 2 3 4 5 6 7 8 9 10 jack queen king ace}
+  suits = %w{ diamonds hearts clubs spades}
+  cards = ranks.product(suits).map { |card| card.join(" of ") }
+  session[:deck] = cards.shuffle
+  session[:player_cards] = []
+  session[:dealer_cards] = []
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  @player_stay = false
+
+  erb :game
 end
 
 get '/new_player' do
@@ -24,6 +36,22 @@ post '/new_player' do
   redirect '/'
 end
 
+get '/welcome' do
 
+end
+
+post '/game/hit' do
+  session[:player_cards] << session[:deck].pop
+
+  erb :game
+end
+
+post '/game/stay' do
+  session[:player_turn] = false
+  session[:dealer_cards] << session[:deck].pop
+  @player_stay = true
+
+  erb :game
+end
 
 
